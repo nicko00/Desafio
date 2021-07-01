968fedfeb9672aeb61f81e271a434f4f0fa351b1
nicko00 <nickminevilil61@gmail.com>
2021-06-30 23:45:56 -0400

    Commit
    ajustes para que as variáveis de min e sec peguem o valor certo, onde minDOM e secDOM serão apenas para, como propriamente dito, mudar no DOM em seu innerHTML de acordo com os cálculos da contagem na regressiva() com as variáveis realmente numéricas, que, futuramente, fará o dever de iniciar e reiniciar o cronometro, tendo 25min ou 5min, pois pausa() será apenas para mudar os minutos, regressiva() fará todo o trabalho.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-var min = parseInt(document.getElementsByClassName('min')[0].innerHTML)
-var sec = parseInt(document.getElementsByClassName('sec')[0].innerHTML)
+var minDOM = document.getElementsByClassName('min')[0]
+var secDOM = document.getElementsByClassName('sec')[0]
+var min = parseInt(minDOM.innerHTML)
+var sec = parseInt(secDOM.innerHTML)
 var contagem = document.getElementsByClassName('aside-diario')[0]
-var cont
+var cont = 0
 var inicio = document.getElementById('iniciar')
 var intervalo = document.getElementById('intervalo')
 var descanso =  document.getElementsByClassName('descanso')[0]
 
-inicio.addEventListener("click", regressiva())
-intervalo.addEventListener("click", pausa())
-descanso.addEventListener("click", descansar())
+contagem.innerHTML += cont
+
+function regressiva(){
+    
+}
+
+function pausa(){
+    minDOM.innerHTML = '5'
+    min = parseInt(minDOM.innerHTML)
+}
+
+function descansar(){
+
+}
