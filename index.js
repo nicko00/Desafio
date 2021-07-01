4b4c99d0e7766c919e4d0f55e5e08a4ebd322467
nicko00 <nickminevilil61@gmail.com>
2021-07-01 01:01:21 -0400

    Commit 7
    (esqueci de numerar o 6 commit);
    feito o cronômetro na regressiva() no back-end;

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var minDOM = document.getElementsByClassName('min')[0]
-var secDOM = document.getElementsByClassName('sec')[0]
-var min = parseInt(minDOM.innerHTML)
-var sec = parseInt(secDOM.innerHTML)
+var minDOM = document.getElementsByClassName('min')[0].innerHTML
+var secDOM = document.getElementsByClassName('sec')[0].innerHTML
+var min = parseInt(minDOM)
+var sec = parseInt(secDOM)
 var contagem = document.getElementsByClassName('aside-diario')[0]
 var cont = 0
 var inicio = document.getElementById('iniciar')
@@ -11,7 +11,21 @@ var descanso =  document.getElementsByClassName('descanso')[0]
 contagem.innerHTML += cont
 
 function regressiva(){
-    
+    min = parseInt(minDOM)
+    sec = parseInt(secDOM)
+    if(min > 0 || sec > 0){
+        if(sec == 0){
+            sec = 59
+            min -= 1
+        }
+        else{
+            sec -= 1
+        }
+        
+        setTimeout(function(){
+            regressiva()
+        }, 1000)
+    }
 }
 
 function pausa(){
@@ -20,5 +34,4 @@ function pausa(){
 }
 
 function descansar(){
-
 }
